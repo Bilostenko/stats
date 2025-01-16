e0cb7d7b1e26e165f7c606475d9a499b6a159bda
Bilostenko <bilostenkod@gmail.com>
2025-01-16 17:56:33 +0200

    add some files .ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-import fs from 'fs'
+import { CsvFileReader } from "./CsvFileReader"
+import { MatchResult } from "./MatchResult"
+
+const reader = new CsvFileReader('football.matches.csv')
+reader.read()
+
+let manUnitedWins = 0
+
+for (let match of reader.data) {
+    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++
+    } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++
+    }
+}
+
